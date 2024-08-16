5b7001b1f8bbe51f5e26f65cea7874066fdf05ff
navid <navid.kavian@ecole.ensicaen.fr>
2024-08-16 18:57:51 +0200

    test

---
+++ chesscasa/src/firebase.js
@@ -0,0 +1,43 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInAnonymously } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import {getDatabase, ref, remove, onDisconnect} from "firebase/database"
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAuNwRUJ8Uxjp1uS_KIYb0ehkuz03OCZAQ",
+    authDomain: "chesscasa-f1975.firebaseapp.com",
+    projectId: "chesscasa-f1975",
+    storageBucket: "chesscasa-f1975.appspot.com",
+    messagingSenderId: "66225634648",
+    appId: "1:66225634648:web:2144bd7d9bce68e620792d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const database = getDatabase();
+
+const auth = getAuth()
+
+let uid = ""
+signInAnonymously(auth)
+    .then(user => {
+        uid = user.user.uid
+        if (uid) {
+            const presenceRef = ref(database, `users/${uid}/disconnected`)
+            remove(presenceRef).then()
+            onDisconnect(presenceRef).set(true).then()
+        }
+    })
+    .catch((error) => {
+        const errorCode = error.code
+        const errorMessage = error.message
+        console.log(errorCode)
+        console.log(errorMessage)
+    });
+
+export const getUid = () => uid
